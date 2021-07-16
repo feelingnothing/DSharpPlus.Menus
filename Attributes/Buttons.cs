7551a5e6388216615696e28b215245791705f466
dolabaeb <viniborg6337@gmail.com>
2021-07-16 05:21:49 +0300

    Add emoji support

--- Attributes/Buttons.cs
+++ Attributes/Buttons.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DSharpPlus.Entities;
 
 namespace DSharpPlus.Menus.Attributes
 {
@@ -8,40 +9,42 @@ namespace DSharpPlus.Menus.Attributes
         internal Guid Id = Guid.NewGuid();
         public ButtonStyle Style { get; }
         public string Label { get; }
+        public DiscordComponentEmoji Emoji { get; }
         public bool Disabled { get; }
 
-        public ButtonAttribute(ButtonStyle style, string label, bool disabled = false)
+        public ButtonAttribute(ButtonStyle style, string label, bool disabled = false, string? emoji = null)
         {
             Style = style;
             Label = label;
+            Emoji = new DiscordComponentEmoji(emoji);
             Disabled = disabled;
         }
     }
 
     public class PrimaryButtonAttribute : ButtonAttribute
     {
-        public PrimaryButtonAttribute(string label, bool disabled = false) : base(ButtonStyle.Primary, label, disabled)
+        public PrimaryButtonAttribute(string label, bool disabled = false, string? emoji = null) : base(ButtonStyle.Primary, label, disabled, emoji)
         {
         }
     }
 
     public class SecondaryButtonAttribute : ButtonAttribute
     {
-        public SecondaryButtonAttribute(string label, bool disabled = false) : base(ButtonStyle.Secondary, label, disabled)
+        public SecondaryButtonAttribute(string label, bool disabled = false, string? emoji = null) : base(ButtonStyle.Secondary, label, disabled, emoji)
         {
         }
     }
 
     public class DangerButtonAttribute : ButtonAttribute
     {
-        public DangerButtonAttribute(string label, bool disabled = false) : base(ButtonStyle.Danger, label, disabled)
+        public DangerButtonAttribute(string label, bool disabled = false, string? emoji = null) : base(ButtonStyle.Danger, label, disabled, emoji)
         {
         }
     }
 
     public class SuccessButtonAttribute : ButtonAttribute
     {
-        public SuccessButtonAttribute(string label, bool disabled = false) : base(ButtonStyle.Success, label, disabled)
+        public SuccessButtonAttribute(string label, bool disabled = false, string? emoji = null) : base(ButtonStyle.Success, label, disabled, emoji)
         {
         }
     }
