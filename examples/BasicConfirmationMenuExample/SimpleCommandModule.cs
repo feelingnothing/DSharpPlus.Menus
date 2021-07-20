16d6d691dcd56de3e24be5b4cd051df92a164848
dolabaeb <viniborg6337@gmail.com>
2021-07-21 02:44:59 +0300

    Add new example

---
+++ examples/BasicConfirmationMenuExample/SimpleCommandModule.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Menus;
+using DSharpPlus.Menus.Attributes;
+using DSharpPlus.Menus.Entities;
+
+namespace BasicConfirmationMenuExample
+{
+    public class ConfirmationMenu : Menu
+    {
+        private readonly ulong member;
+        public TaskCompletionSource<bool> Tcs { get; } = new();
+
+        public ConfirmationMenu(ulong member, TimeSpan timeout, DiscordClient client) : base(client)
+        {
+            var token = new CancellationTokenSource(timeout);
+            token.Token.Register(async () => await StopAsync(true));
+            this.member = member;
+        }
+
+        public override Task<bool> CanBeExecuted(ComponentInteractionCreateEventArgs args) =>
+            Task.FromResult(args.User.Id == member);
+
+        private async void SetValue(ComponentInteractionCreateEventArgs args, bool value)
+        {
+            await args.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+            Tcs.TrySetResult(value);
+            await StopAsync();
+        }
+
+        [SuccessButton("Confirm")]
+        public async Task ConfirmAsync(ComponentInteractionCreateEventArgs args) => SetValue(args, true);
+
+        [DangerButton("Deny")]
+        public async Task DenyAsync(ComponentInteractionCreateEventArgs args) => SetValue(args, false);
+
+        public override Task StopAsync(bool timeout = false)
+        {
+            if (timeout) Tcs.TrySetResult(false);
+            return base.StopAsync(timeout);
+        }
+    }
+
+    [Group("menu")]
+    public class SimpleCommandModule : BaseCommandModule
+    {
+        [GroupCommand]
+        public async Task SomethingImportantAsync(CommandContext ctx)
+        {
+            var menu = new ConfirmationMenu(ctx.Member.Id, TimeSpan.FromSeconds(10), ctx.Client);
+            await menu.StartAsync();
+            await ctx.RespondAsync(new DiscordMessageBuilder().AddMenu(menu).WithContent("Are you confirming this action?"));
+            var result = await menu.Tcs.Task;
+            await ctx.RespondAsync(result ? "You have confirmed this action!" : "You have denied this action.");
+        }
+    }
+}
---
+++ examples/BasicConfirmationMenuExample/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Menus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BasicConfirmationMenuExample
+{
+    public static class Startup
+    {
+        private static readonly string Token = Environment.GetEnvironmentVariable("TOKEN");
+        private static readonly string[] Prefixes = {".", "~", "\\"};
+
+        public static async Task Main()
+        {
+            var services = ConfigureServices();
+            var client = services.GetRequiredService<DiscordClient>();
+            var commands = services.GetRequiredService<CommandsNextExtension>();
+
+            client.UseMenus();
+            commands.RegisterCommands<SimpleCommandModule>();
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(_ => new DiscordClient(new DiscordConfiguration
+                {
+                    Token = Token,
+                    Intents = DiscordIntents.AllUnprivileged
+                }))
+                .AddSingleton(s =>
+                {
+                    var client = s.GetRequiredService<DiscordClient>();
+                    return client.UseCommandsNext(new CommandsNextConfiguration
+                    {
+                        StringPrefixes = Prefixes,
+                        Services = s
+                    });
+                })
+                .BuildServiceProvider(true);
+        }
+    }
+}
