4a6d0a3586bc9dc721e7af885ef3ebec077c6408
feelingnothing <47575622+feelingnothing@users.noreply.github.com>
2021-08-01 08:51:54 +0300

    Add support for sharded client menu registration

--- DSharpPlus.Menus/ExtensionMethods.cs
+++ DSharpPlus.Menus/ExtensionMethods.cs
@@ -9,6 +9,14 @@
             return ext;
         }
 
+        public static MenusExtension UseMenus(this DiscordShardedClient client, MenusConfiguration? configuration = null)
+        {
+            var ext = new MenusExtension(configuration ?? new MenusConfiguration());
+            foreach (var (_, shard) in client.ShardClients)
+                shard.AddExtension(ext);
+            return ext;
+        }
+
         public static MenusExtension GetMenus(this DiscordClient client) => client.GetExtension<MenusExtension>();
     }
 }
